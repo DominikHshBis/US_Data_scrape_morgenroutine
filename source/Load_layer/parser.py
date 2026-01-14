d344e86632e8220cabb404b3f1469b24600e9cec
Dominik Ballentin <dominik.ballentin@stud.hs-hannover.de>
2026-01-14 17:36:15 +0000

    parser_mini

--- source/Load_layer/parser.py
+++ source/Load_layer/parser.py
@@ -1,3 +1,18 @@
+import polars as pl
+
 class Parser:
     def parse(self, raw):
-        pass
+        # raw kann JSON, CSV-String, Dict, Liste, HTML etc. sein
+        df = pl.DataFrame(raw)
+
+        # Beispiel: Spalten normalisieren
+        df = df.rename({"Date": "date", "Close": "close"})
+
+        # Beispiel: Typen setzen
+        df = df.with_columns([
+            pl.col("date").str.strptime(pl.Date),
+            pl.col("close").cast(pl.Float64)
+        ])
+
+        return df
+
